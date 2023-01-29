206b35ee836b43a12706e8d9e301690dfa1647fb
The-Schwiz <113313870+The-Schwiz@users.noreply.github.com>
2023-01-29 16:18:22 -0500

    feature-signup: Improve seed script to drop tables before seeding

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,14 @@
+const path = require('path');
 const sequelize = require('../config/connection');
 const seedUsers = require('./user-seeds');
 const seedProfiles = require('./profile-seeds');
 const seedLinks = require('./links-seeds');
 
 sequelize.sync({ force: false }).then(async () => {
+  // await sequelize.query(`source ${path.resolve(__dirname, '../db/schema.sql')}`);
+  await sequelize.query('DELETE FROM links');
+  await sequelize.query('DELETE FROM profile');
+  await sequelize.query('DELETE FROM user');
   await seedUsers();
   await seedProfiles();
   await seedLinks();
