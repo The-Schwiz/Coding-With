04e608690cf6535e1f1dc8f530acaacf8047ede8
The-Schwiz <113313870+The-Schwiz@users.noreply.github.com>
2023-01-31 20:01:28 -0500

    implement edit and update view and endpoints

--- controllers/profile.js
+++ controllers/profile.js
@@ -42,5 +42,87 @@ const getProfilePage = async (req, res) => {
 const createProfile = async (req, res) => {
 }
 
+const editProfilePage = async (req, res) => {
+  const profileId = req.params.id;
+  // query the profile from the db
+  const dbProfile = await Profile.findOne({
+      where: {
+        id: profileId,
+      },
+      attributes: [
+          'id',
+          "picture",
+          'name',
+          'bio',
+          'location',
+          'experience',
+          'tech',
+          'other'],
+      include: [
+        {
+          model: Links,
+          attributes: ['id', 'linkedin', 'github', 'ig', 'twitter', 'email', 'website'],
+        },
+        {
+          model: User,
+          attributes: ['email']
+        }
+      ]
+  });
 
-module.exports = { getProfilePage };
+  // check the profile exists
+  if (!dbProfile) {
+      return res.status(404).send("Profile Not Found");
+  }
+  // sanatize the profile db object
+  const profile = dbProfile.get({ plain: true });
+  return res.render('editProfile', { profile });
+}
+
+
+const updateProfile = async (req, res) => {
+  const profileId = req.params.id;
+
+  let result = await Profile.update({
+    picture: req.body.picture,
+    name: req.body.name,
+    bio: req.body.bio,
+    location: req.body.location,
+    experience: req.body.experience,
+    tech: req.body.tech,
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  );
+  let affectedRowsCount = result[0];
+  if (affectedRowsCount === 0) {
+    return res.status(404).send("Profile Not Found");
+  }
+
+  // update links
+  result = await Links.update({
+    linkedin: req.body.linkedin,
+    github: req.body.github,
+    ig: req.body.ig,
+    twitter: req.body.twitter,
+    email: req.body.email,
+    website: req.body.website,
+    },
+    {
+      where: {
+        profile_id: req.params.id
+      }
+    }
+  );
+  affectedRowsCount = result[0];
+  if (affectedRowsCount === 0) {
+    return res.status(404).send("Links Not Found");
+  }
+
+  return res.redirect(`/profiles/${profileId}`);
+}
+
+module.exports = { getProfilePage, editProfilePage, updateProfile };
